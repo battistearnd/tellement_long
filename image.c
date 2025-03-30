4dd86721a20e4d02b812841a73d9c2e8bb2e9c06
battistearnd <161437207+battistearnd@users.noreply.github.com>
2025-03-30 22:13:46 +0200

    Create image.c

---
+++ image.c
@@ -0,0 +1,31 @@
+static int	open_ground_coins_door(t_game *game)
+{
+	game->textures.ground = mlx_xpm_file_to_image(game->mlx, \
+	"assets/dirt.xpm", &game->width, &game->height);
+	if (game->textures.ground == NULL)
+		return (1);
+	game->textures.coin = mlx_xpm_file_to_image(game->mlx, \
+	"assets/coin.xpm", &game->width, &game->height);
+	if (game->textures.coin == NULL)
+		return (1);
+	game->textures.door = mlx_xpm_file_to_image(game->mlx, \
+	"assets/door.xpm", &game->width, &game->height);
+	if (game->textures.door == NULL)
+		return (1);
+	game->textures.wall = mlx_xpm_file_to_image(game->mlx, \
+	"assets/wall.xpm", &game->width, &game->height);
+	if (game->textures.wall == NULL)
+		return (1);
+	return (0);
+}
+
+void	open_images(t_game *game)
+{
+	game->height = HEIGHT;
+	game->width = WIDTH;
+	game->current_frame = 0;
+	game->player.player_idle_right_1 = mlx_xpm_file_to_image(game->mlx, \
+	"assets/player_idle_right_1.xpm", &game->width, &game->height);
+	if (open_ground_coins_door(game) == 1)
+		write(1, "failed to open the image\n", 25);
+}
